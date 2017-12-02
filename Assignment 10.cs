31af891751c170e7282b05cd08a85c9f9f3acf0d
Ignacio Navarro <inavarro04@yahoo.com>
2017-12-02 14:15:25 -0800

    upload of all assignments for final grading

---
+++ Assignment 10.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            do
+            {
+                Console.WriteLine("Please enter a number greater than 0.\n");
+                num = Convert.ToInt32(Console.ReadLine());
+
+                if (num <= 0)
+                {
+                    Console.WriteLine("invalid selection, try again.\n");
+                }
+
+
+            } while (num <= 0);
+
+            Console.WriteLine("The prime factors of " + num + " are: \n\n");
+            PrimeFactorizer(num);
+
+        }
+        static void PrimeFactorizer(int num)
+        {
+            while (num % 2 == 0)
+            {
+                Console.WriteLine("2\n");
+                num = num / 2;
+            }
+            for (int i = 3; i <= Math.Sqrt(num); i++)
+            {
+                while (num % i == 0)
+                {
+                    Console.WriteLine(i + "\n");
+                    num = num / i;
+                }
+            }
+
+            if (num > 2)
+                Console.WriteLine(num + "\n");
+
+        }
+
+       
+    }
+}
---
+++ Assignment 14.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_14
+{
+    class Program
+    {
+        static Stack<int> callStack = new Stack<int>();
+
+        public static int FibonacciSeries(int n)
+        {
+
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+
+            return FibonacciSeries(n - 1) + FibonacciSeries(n - 2);
+        }
+        public static void printCallStack()
+        {
+            Stack<int> stackToPrint = new Stack<int>();
+           
+            int size = callStack.Count();
+
+            for (int i = 0; i < size; i++)
+            {
+                stackToPrint.Push(callStack.Pop());
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                callStack.Push(stackToPrint.Pop());
+
+                Console.Write(callStack.Peek() + ", ");
+
+            }
+
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the length of the Fibonacci Series: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("{0}  ", FibonacciSeries(i));
+                callStack.Push(FibonacciSeries(i));
+                printCallStack();               
+                Console.ReadKey();
+
+            }
+            //Console.ReadKey();
+        }
+
+    }
+
+    
+}
